aacedf66641962ac1f0d7b3392acc2314789eef6
John Allen <john@threedogconsulting.com>
2011-01-11 14:58:58 -0500

    loading response object for all calls, updated readme

--- lib/googl/client.rb
+++ lib/googl/client.rb
@@ -20,29 +20,19 @@ module Googl
     
     def shorten(url)
       Googl::Response.new(self.class.post('/url', :body => "{ \"longUrl\" => \"#{url}\" }").parsed_response)
-      # response = nil
-      # 
-      # # when providing a hash for the body the url is being URI Encoded and causing an error
-      # self.class.post('/url', :body => "{ \"longUrl\" => \"#{url}\" }").each do |r|
-      #   response = Googl::Response.new(r)
-      # end
-      # 
-      # return response
-      
-      # api key not working at this time
-      # self.class.post('/url', :query => @api_key, :body => "{ \"longUrl\" => \"#{url}\" }").parsed_response
+      # Googl::Response.new(self.class.post('/url', :query => @api_key, :body => "{ \"longUrl\" => \"#{url}\" }").parsed_response)
     end
     
     def expand(url)
-      self.class.get('/url', :query => { :shortUrl => url }).parsed_response
+      Googl::Response.new(self.class.get('/url', :query => { :shortUrl => url }).parsed_response)
       # api key not working at this time
-      # self.class.get('/url', :query => @api_key.merge({ :shortUrl => url })).parsed_response
+      # Googl::Response.new(self.class.get('/url', :query => @api_key.merge({ :shortUrl => url })).parsed_response)
     end
     
     def analytics(url, projection = "FULL")
-      self.class.get('/url', :query => { :shortUrl => url, :projection => projection }).parsed_response
+      Googl::Response.new(self.class.get('/url', :query => { :shortUrl => url, :projection => projection }).parsed_response)
       # api key not working at this time
-      # self.class.get('/url', :query => @api_key.merge({ :shortUrl => url, :projection => projection })).parsed_response
+      # Googl::Response.new(self.class.get('/url', :query => @api_key.merge({ :shortUrl => url, :projection => projection })).parsed_response)
     end
   end
 end
