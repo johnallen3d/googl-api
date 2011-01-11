e7c1a98eb1e6e302d90c0ce660e5238fc4c1248b
John Allen <john@threedogconsulting.com>
2011-01-11 14:46:12 -0500

    improved response object support

--- lib/googl/response.rb
+++ lib/googl/response.rb
@@ -1,5 +1,26 @@
 module Googl
   class Response
-    attr_accessor :kind, :shortUrl, :longUrl, :status, :created, :analytics, :raw
+    attr_accessor :kind, :id, :longUrl, :status, :created, :analytics, :raw
+
+    def initialize args
+      # load valuse passed in hash
+      args.each do |k,v|
+        instance_variable_set("@#{k}", v) unless v.nil?
+      end
+      
+      # retain the hash for further inspection
+      self.raw = args
+    end
+
+    # provide helper method for short url (returned as id)
+    def short_url
+      self.id
+    end
+
+    # proive ruby like method for longUrl
+    def long_url
+      self.longUrl
+    end
+
   end
 end
